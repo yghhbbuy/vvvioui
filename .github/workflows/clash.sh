ae588280c5baea1a00e2e5428b27ad7d8e82f71a
yghhbbuy <144499345+yghhbbuy@users.noreply.github.com>
2026-03-14 08:51:39 +0800

    Update clash.sh

--- .github/workflows/clash.sh
+++ .github/workflows/clash.sh
@@ -1,11 +1,17 @@
 sudo -i
+
+
 ls -l /home
 sudo chown -R $USER:$USER /home/$USER
 
+
 rm -rf lost+found/
 
+
 apt update
-
 apt install lftp
-
 apt install tmux
+
+
+SSH_USER=ttt0090
+SSH_PASSWORD='123789'
