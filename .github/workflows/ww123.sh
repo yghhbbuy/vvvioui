d136629a9c10f1758a7c72c927d4312aa2e1c086
yghhbbuy <144499345+yghhbbuy@users.noreply.github.com>
2026-03-16 05:03:56 +0800

    Update FTP credentials and commands in ww123.sh

--- .github/workflows/ww123.sh
+++ .github/workflows/ww123.sh
@@ -15,11 +15,11 @@ lftp -u if0_41271182,BPJhhbAWEAZAT2G ftp://185.27.134.11 -e "set ssl:verify-cert
 
 lftp -u if0_40929541,3vpHv2yq6JT ftp://185.27.134.11 -e "set ftp:passive-mode on; set ssl:verify-certificate no; mirror -R ./wwwww htdocs/230130; bye"
 
-lftp -u if0_40929541,3vpHv2yq6JT ftp://185.27.134.11 -e "set ssl:verify-certificate no; mrm htdocs/230130/*.json.*; bye"
+lftp -u if0_40929541,KUS4FNUnlWjq3 ftp://185.27.134.11 -e "set ssl:verify-certificate no; mrm htdocs/230130/*.json.*; bye"
 lftp -u if0_41226805,Ofv7XTmtfmB ftp://185.27.134.11 -e "set ftp:passive-mode on; set ssl:verify-certificate no; mirror -R ./wwwww htdocs/230130; bye"
 
-lftp -u if0_41226805,Ofv7XTmtfmB ftp://185.27.134.11 -e "set ssl:verify-certificate no; mrm htdocs/230130/*.json.*; bye"
-lftp -u if0_41226805,Ofv7XTmtfmB ftp://185.27.134.11 -e "set ftp:passive-mode on; set ssl:verify-certificate no; mirror -R ./2026 htdocs/2026; bye"
+lftp -u if0_41399455,KUS4FNUnlWjq3 ftp://185.27.134.11 -e "set ssl:verify-certificate no; mrm htdocs/230130/*.json.*; bye"
+lftp -u if0_41399455,KUS4FNUnlWjq3 ftp://185.27.134.11 -e "set ftp:passive-mode on; set ssl:verify-certificate no; mirror -R ./2026 htdocs/2026; bye"
 
 lftp -u if0_41226805,Ofv7XTmtfmB ftp://185.27.134.11 -e "set ssl:verify-certificate no; mrm htdocs/2026/*.pdf.*; bye"
 
