0466d0ea56efe621e7f89096683014289efcbdc4
yghhbbuy <144499345+yghhbbuy@users.noreply.github.com>
2026-05-07 06:19:16 +0800

    Add files via upload

--- .github/workflows/index.php
+++ .github/workflows/index.php
@@ -80,7 +80,7 @@ BODY {
 <a href="https://ttt0090.my-place.us/" style="color:#FF00FF" target="_blank">ttt0090-us</a>|
 
 
-<a href="https://kkkkk.gt.tc" style="color:#FF00FF" target="_blank">kkkkk</a>|
+<a href="https://o365-five.up.railway.app/" style="color:#FF00FF" target="_blank">rail-o365</a>|
 
 
 <a href="https://tpwhokyoqnxs.cloud.cloudcat.one/" style="color:#FF00FF" target="_blank">cloudcat</a>|
